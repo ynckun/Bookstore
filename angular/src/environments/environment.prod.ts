1823b4b05dec1ff4cd855ad0b4f7ad6ab3cbeaa5
ynckun <105028469+ynckun@users.noreply.github.com>
2022-06-11 19:27:15 +0800

    Update environment.prod.ts

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'https://stunning-beignet-b07c62.netlify.app';
+const baseUrl = 'https://zippy-medovik-db6f69.netlify.app';
 
 export const environment = {
   production: true,
