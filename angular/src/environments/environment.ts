14ebd3cc7551e0f86e742c901f8faac531cc8ae5
ynckun <105028469+ynckun@users.noreply.github.com>
2022-06-11 19:29:10 +0800

    Update environment.ts

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'https://stunning-beignet-b07c62.netlify.app';
+const baseUrl = 'https://zippy-medovik-db6f69.netlify.app';
 
 export const environment = {
   production: true,
@@ -10,7 +10,7 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'http://khalidanta-001-site1.dtempurl.com',
+    issuer: 'http://yancykun-001-site1.itempurl.com',
     redirectUri: baseUrl,
     clientId: 'BookStore_App',
     responseType: 'code',
@@ -19,7 +19,7 @@ export const environment = {
   },
   apis: {
     default: {
-      url: 'http://khalidanta-001-site1.dtempurl.com',
+      url: 'http://yancykun-001-site1.itempurl.com',
       rootNamespace: 'Acme.BookStore',
     },
   },
